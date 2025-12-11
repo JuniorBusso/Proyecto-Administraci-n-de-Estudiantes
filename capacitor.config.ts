f01ba7de8106bc5ce8fd758c54572dfa3790ed64
Junior <juniorbusso@gmail.com>
2025-12-11 14:15:59 -0300

    commit inicial

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tuempresa.administracionestudiantes',
+  appName: 'administracion-estudiantes',
+  webDir: 'build'
+};
+
+export default config;
